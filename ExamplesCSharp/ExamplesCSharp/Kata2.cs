15dacb20419a410a81eec4f2a3ada4908c8cc152
David Mayo <jekthor@gmail.com>
2020-05-22 17:09:11 -0500

    add kata 2

---
+++ ExamplesCSharp/ExamplesCSharp/Kata2.cs
@@ -0,0 +1,15 @@
+namespace ExamplesCSharp
+{
+    public class Kata2
+    {
+        //Convert number to string method
+        public string NumberToString(int num)
+        {
+            return num.ToString();
+        }
+
+        //this is other way to meke the same function 
+
+        //public static string NumberToString(int num) => num.ToString();
+    }
+}
--- ExamplesCSharp/ExamplesCSharp/Program.cs
+++ ExamplesCSharp/ExamplesCSharp/Program.cs
@@ -13,6 +13,12 @@ namespace ExamplesCSharp
             var outtext = kata1.Solution("tcejorp");
 
             Console.WriteLine(outtext);
+
+            Kata2 kata2 = new Kata2();
+
+            string number = kata2.NumberToString(1);
+
+            Console.WriteLine(number);
         }
     }
 }
